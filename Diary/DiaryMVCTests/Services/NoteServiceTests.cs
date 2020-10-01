da4fbe08ce180a5bfa08ca57df82619d8306ad62
Vlad0531 <foreignforger@gmail.com>
2020-10-01 23:32:58 +0400

    DTT-35 #comment added NoteService unit tests. Deleted DAL unit tests project

---
+++ Diary/DiaryMVCTests/Services/NoteServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using DiaryDAL.Entities;
+using DiaryDAL.Repositories;
+using DiaryMVC.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using System.Collections.Generic;
+
+namespace DiaryMVCTests.Services
+{
+    [TestClass]
+    public class NoteServiceTests
+    {
+        [TestMethod]
+        public void ShouldGetNotes()
+        {
+            var notes = new List<Note>() { new Note() { Id = 1, Title = "test title 1" }, new Note() { Id = 2, Title = "another test title 2" } };
+
+            var noteRepositoryMock = MockRepository.GenerateMock<INoteRepository>();
+            noteRepositoryMock.Expect(repo => repo.GetNotes()).Return(notes);
+
+            var noteService = new NoteService(noteRepositoryMock);
+
+            var resultNotes = noteService.GetNotes();
+
+            Assert.AreEqual(notes, resultNotes, "should return correct notes");
+            noteRepositoryMock.VerifyAllExpectations();
+        }
+    }
+}
